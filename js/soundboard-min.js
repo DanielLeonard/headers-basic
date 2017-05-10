5bd38ff34e90f75eea823d54680f2583e3da84a0
DanielLeonard <dleonard96@outlook.com>
2017-05-10 11:25:23 +0100

    basic

---
+++ js/soundboard-min.js
@@ -0,0 +1,1 @@
+function jack(){var n=document.getElementById("rum");n.play()}function trump(){var n=document.getElementById("china");n.play()}function croc(){var n=document.getElementById("knife");n.play()}function marty(){var n=document.getElementById("backtofuture");n.play()}function indiana(){var n=document.getElementById("plane");n.play()}
---
+++ js/soundboard.js
@@ -0,0 +1,24 @@
+function jack(){
+     var audio = document.getElementById("rum");
+     audio.play();
+               }
+
+function trump(){
+     var audio = document.getElementById("china");
+     audio.play();
+               }
+
+function croc(){
+    var audio = document.getElementById("knife");
+    audio.play();
+              }
+
+function marty(){
+   var audio = document.getElementById("backtofuture");
+   audio.play();
+             }
+
+function indiana(){
+  var audio = document.getElementById("plane");
+  audio.play();
+            }
